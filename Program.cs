6ed92adf7a24ac8dce08d46580249b01dbe3a0e3
danyelchidera <danyelchidera1@gmail.com>
2022-06-19 13:16:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.NET.Repositories
+{
+    internal interface IRepository
+    {
+    }
+}
